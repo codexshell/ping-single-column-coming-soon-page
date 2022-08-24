326b677ee68b86a1ea21957386287384d748dd49
silvanos <ericoderito@gmail.com>
2022-08-24 13:58:03 +0300

    style(button.svelte): style button component

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,3 +1,24 @@
 <button type="submit">Notify Me</button>
 
-<style></style>
+<style>
+	button {
+		background-color: theme('colors.p-blue');
+		width: 100%;
+		border-radius: theme('borderRadius.full');
+		color: white;
+		font-size: theme('fontSize.xs');
+		padding-block: theme('padding.2');
+		font-weight: 600;
+    border: solid theme("colors.p-blue");
+	}
+
+	button:hover, button:focus-visible {
+		filter: brightness(125%);
+	}
+
+  button:active {
+    background-color: white;
+    color: theme("colors.p-blue");
+    border: solid;
+  }
+</style>
