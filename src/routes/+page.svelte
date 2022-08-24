02892e1213242e13aebeaf960dae12d29e21cc42
silvanos <ericoderito@gmail.com>
2022-08-24 12:32:54 +0300

    style(page): style h1 and p tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,29 @@
 	<meta name="description" content="coming soon page demo app" />
 </svelte:head>
 
-<section>
+<section class="flow">
 	<h1>We are launching <span>soon!</span></h1>
 	<p>Subscribe and get notified</p>
 	<Email />
 	<img src="/illustration-dashboard.png" alt="illustration dashboard" />
 </section>
 
-<style></style>
+<style>
+	section {
+		margin-top: 2.5rem;
+		text-align: center;
+	}
+
+	h1 {
+		color: theme('colors.n-gray');
+	}
+
+	h1 span {
+		color: theme('colors.n-very-dark-blue');
+		font-weight: 700;
+	}
+
+	p {
+		font-size: theme('fontSize.xs');
+	}
+</style>
