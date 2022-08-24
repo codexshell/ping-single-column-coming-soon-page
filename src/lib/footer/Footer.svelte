07a0533f18e6b6c8eaf46c78d8f69dc5fdc68ff9
silvanos <ericoderito@gmail.com>
2022-08-24 11:24:58 +0300

    feat(css): add font awesome support
    add facebook, twitter and instagram social icons

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,7 +1,18 @@
 <script></script>
 
 <footer>
-  <p>&copy; Copyright Ping. All rights reserved.</p>
+	<div class="social-icons">
+		<div class="social-icon">
+			<i class="fa-brands fa-facebook-f" />
+		</div>
+		<div class="social-icon">
+			<i class="fa-brands fa-twitter" />
+		</div>
+		<div class="social-icon">
+			<i class="fa-brands fa-instagram" />
+		</div>
+	</div>
+	<p>&copy; Copyright Ping. All rights reserved.</p>
 </footer>
 
 <style></style>
