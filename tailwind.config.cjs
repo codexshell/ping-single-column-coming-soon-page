375a6aa78b5d65475a5f9186845d8a4e1cf8ebe0
silvanos <ericoderito@gmail.com>
2022-08-24 11:02:35 +0300

    feat(css): generate utility classes
    Use tailwind css to generate custom utility classes.
    Do this by extending tailwind css in tailwind.config.cjs.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'p-blue': 'hsl(223, 87%, 63%)',
+				's-pale-blue': 'hsl(223, 100%, 88%)',
+				's-light-red': 'hsl(354, 100%, 66%)',
+				'n-gray': 'hsl(0, 0%, 59%)',
+				'n-very-dark-blue': 'hsl(209, 33%, 12%)'
+			},
+			fontFamily: {
+				'libre-franklin': ['Libre Franklin', 'sans-serif']
+			},
+			fontSize: {
+				'body-copy': '20px'
+			}
+		}
 	},
 	plugins: []
 };
